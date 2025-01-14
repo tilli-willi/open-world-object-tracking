5b59e9b6a53049c219c5e103981d617e78a4c8dc
Daniil Staroverov <danil.staroverov@gmail.com>
2025-01-14 13:18:26 +0000

    Rename to Hyp-OW

--- Hyp-OW-burst-eval/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-GPUS_PER_NODE=4 ./tools/run_dist_launch.sh 4 configs/RUN_HIERARCHICAL_SPLIT.sh
--- Hyp-OW-burst-eval/run_eval.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-GPUS_PER_NODE=4 ./tools/run_dist_launch.sh 4 configs/EVAL_HIERARCHICAL_SPLIT.sh
-
-##  for other splits
-##  configs/EVAL_OWOD_SPLIT.sh
-##  configs/EVAL_OWDETR_SPLIT.sh 
